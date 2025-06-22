b797a15da124c520d34846499b8badc6d9e5f1e2
huhdy32 <mong3268@gmail.com>
2025-06-22 22:39:43 +0900

    feat(board): 게시글 등록 이벤트 발행기능 구현

--- domain/mathrank-board/src/main/java/kr/co/mathrank/domain/board/service/PostRegisterService.java
+++ domain/mathrank-board/src/main/java/kr/co/mathrank/domain/board/service/PostRegisterService.java
@@ -14,6 +14,9 @@ import kr.co.mathrank.domain.board.entity.FreePost;
 import kr.co.mathrank.domain.board.entity.Post;
 import kr.co.mathrank.domain.board.entity.ProblemQuestionPost;
 import kr.co.mathrank.domain.board.entity.PurchasePost;
+import kr.co.mathrank.domain.board.outbox.EventType;
+import kr.co.mathrank.domain.board.outbox.Outbox;
+import kr.co.mathrank.domain.board.outbox.OutboxEventPublisher;
 import kr.co.mathrank.domain.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -22,26 +25,39 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostRegisterService {
 	private final PostRepository postRepository;
+	private final OutboxEventPublisher outboxEventPublisher;
 
 	public String save(@NotNull @Valid final FreePostCreateCommand command) {
-		final Post post = new FreePost(command.title(), command.content(), command.memberId(), LocalDateTime.now(),
+		final LocalDateTime now = getCurrentTime();
+		final Post post = new FreePost(command.title(), command.content(), command.memberId(), now,
 			command.images());
+		post.setOutbox(new Outbox(EventType.FREE_POST_CREATED_EVENT, now));
 		return savePost(post);
 	}
 
 	public String save(@NotNull @Valid final ProblemQuestionPostCreateCommand command) {
-		final Post post = new ProblemQuestionPost(command.title(), command.content(), command.memberId(),
-			LocalDateTime.now(), command.images(), command.problemId());
+		final LocalDateTime now = getCurrentTime();
+		final Post post = new ProblemQuestionPost(command.title(), command.content(), command.memberId(), now,
+			command.images(), command.problemId());
+		post.setOutbox(new Outbox(EventType.PROBLEM_POST_CREATED_EVENT, now));
 		return savePost(post);
 	}
 
 	public String save(@NotNull @Valid final PurchasePostCreateCommand command) {
-		final Post post = new PurchasePost(command.title(), command.content(), command.memberId(),
-			LocalDateTime.now(), command.images(), command.purchaseId());
+		final LocalDateTime now = getCurrentTime();
+		final Post post = new PurchasePost(command.title(), command.content(), command.memberId(), now,
+			command.images(), command.purchaseId());
+		post.setOutbox(new Outbox(EventType.PURCHASE_POST_CREATED_EVENT, now));
 		return savePost(post);
 	}
 
+	private LocalDateTime getCurrentTime() {
+		return LocalDateTime.now();
+	}
+
 	private String savePost(final Post post) {
-		return postRepository.save(post).getId();
+		final String id = postRepository.save(post).getId();
+		outboxEventPublisher.publishMessage(post);
+		return id;
 	}
 }
