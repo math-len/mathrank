cda0e9481125a64d8a7e230a92754e7f60d83db4
huhdy32 <mong3268@gmail.com>
2025-07-09 21:08:40 +0900

    feat(problem): 새로운 과정 저장 기능 추가

--- domain/mathrank-problem/src/main/java/kr/co/mathrank/domain/problem/repository/CourseRepository.java
+++ domain/mathrank-problem/src/main/java/kr/co/mathrank/domain/problem/repository/CourseRepository.java
@@ -1,9 +1,16 @@
 package kr.co.mathrank.domain.problem.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kr.co.mathrank.domain.problem.entity.Course;
 import kr.co.mathrank.domain.problem.entity.Path;
 
 public interface CourseRepository extends JpaRepository<Course, Path> {
+	@Query("SELECT c FROM Course c WHERE c.path.path LIKE CONCAT(:path, '%')")
+	List<Course> queryCourseStartsWith(@Param("path") final String path, Pageable pageable);
 }
---
+++ domain/mathrank-problem/src/main/java/kr/co/mathrank/domain/problem/service/CourseService.java
@@ -0,0 +1,50 @@
+package kr.co.mathrank.domain.problem.service;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import kr.co.mathrank.domain.problem.dto.CourseRegisterCommand;
+import kr.co.mathrank.domain.problem.entity.Course;
+import kr.co.mathrank.domain.problem.entity.Path;
+import kr.co.mathrank.domain.problem.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Validated
+@RequiredArgsConstructor
+public class CourseService {
+	private final CourseRepository courseRepository;
+
+	@Transactional
+	public String register(@NotNull @Valid final CourseRegisterCommand command) {
+		final Path parentPath = new Path(command.parentPath());
+		final Course longestPath = getLatestPathCourse(parentPath);
+
+		// 첫 삽입일 경우
+		if (command.parentPath().isBlank() && longestPath == null) {
+			return courseRepository.save(Course.of(command.courseName(), new Path())).getPath().getPath();
+		}
+
+		// 존재하지 않는 부모 조회
+		if (!command.parentPath().isBlank() && longestPath == null) {
+			throw new IllegalArgumentException("존재하지 않는 상위 path");
+		}
+
+		final Path childPath = parentPath.nextChild(longestPath.getPath());
+
+		final Course course = Course.of(command.courseName(), childPath);
+		return courseRepository.save(course).getPath().getPath();
+	}
+
+	private Course getLatestPathCourse(final Path path) {
+		return courseRepository.queryCourseStartsWith(path.getPath(),
+				PageRequest.ofSize(1).withSort(Sort.by(Sort.Direction.DESC, "path")))
+			.stream()
+			.findFirst().orElse(null);
+	}
+}
