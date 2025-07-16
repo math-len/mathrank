f39e90de2b8be98831624e75b5b124387c59df23
huhdy32 <mong3268@gmail.com>
2025-07-16 11:24:28 +0900

    docs(api-problem): 문제 등록,수정,삭제 설명 추가

--- app/api/mathrank-problem-api/src/main/java/kr/co/mathrank/app/api/problem/ProblemController.java
+++ app/api/mathrank-problem-api/src/main/java/kr/co/mathrank/app/api/problem/ProblemController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.mathrank.app.api.common.authentication.Authorization;
@@ -22,10 +23,11 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "problem")
+@Tag(name = "문제 API")
 public class ProblemController {
 	private final ProblemService problemService;
 
+	@Operation(summary = "문제 등록 API", description = "이 API를 통해 등록된 문제들은, 서버내의 다른 API ( 개별문제, 시험문제 )에서 조합하여 사용할 수 있습니다.")
 	@PostMapping("/api/v1/problem")
 	@Authorization(values = Role.ADMIN)
 	public ResponseEntity<Void> register(
@@ -38,6 +40,7 @@ public class ProblemController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "문제 수정 API", description = "문제를 수정합니다. 바꾸지 않고자 하는 필드가 있다면, 해당 필드를 원본 값으로 채워야합니다.")
 	@PutMapping("/api/v1/problem")
 	@Authorization(values = Role.ADMIN)
 	public ResponseEntity<Void> update(
@@ -50,6 +53,7 @@ public class ProblemController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "문제 삭제 API", description = "다른 도메인에서 사용된 문제는 모두 무효화 됩니다.")
 	@DeleteMapping("/api/v1/problem")
 	@Authorization(values = Role.ADMIN)
 	public ResponseEntity<Void> delete(
