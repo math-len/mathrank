3a0d6d664bdc36c7a040f1e3731de4f0ed18ea3b
huhdy32 <mong3268@gmail.com>
2025-07-16 11:53:14 +0900

    docs(api-school): 학교 조회 api 설명 추가

--- app/api/mathrank-school-read-api/src/main/java/kr/co/mathrank/app/api/school/SchoolQueryController.java
+++ app/api/mathrank-school-read-api/src/main/java/kr/co/mathrank/app/api/school/SchoolQueryController.java
@@ -8,17 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.mathrank.client.external.school.RequestType;
 import kr.co.mathrank.client.external.school.SchoolClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Tag(name = "학교 정보 API", description = "학교 정보를 조회하는 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 public class SchoolQueryController {
 	private final SchoolClient schoolClient;
 
+	@Operation(summary = "학교의 이름을 통한 조회 API", description = "schoolName 을 입력하지 않을 시, 빈값으로 사용됩니다.")
 	@GetMapping("/api/v1/schools")
 	public ResponseEntity<List<SchoolResponse>> loadInfos(
 		@RequestParam(required = false)
