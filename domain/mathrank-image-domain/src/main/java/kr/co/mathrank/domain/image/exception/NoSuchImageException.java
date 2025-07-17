c3b5e149da2bdcedc35430d33b59bb2c51bd28d0
huhdy32 <mong3268@gmail.com>
2025-07-17 18:08:07 +0900

    refactor(image): 공통예외로 감싼다
    변경 전:
    예외로 인한 500 에러 발생
    
    변경 후:
    커스텀 예외를 통한 http 응답 코드 지정

--- domain/mathrank-image-domain/src/main/java/kr/co/mathrank/domain/image/exception/NoSuchImageException.java
+++ domain/mathrank-image-domain/src/main/java/kr/co/mathrank/domain/image/exception/NoSuchImageException.java
@@ -1,4 +1,12 @@
 package kr.co.mathrank.domain.image.exception;
 
-public class NoSuchImageException extends RuntimeException {
+import kr.co.mathrank.common.exception.HttpMathRankException;
+
+public class NoSuchImageException extends HttpMathRankException {
+	private static final int HTTP_STATUS_CODE = 404; // 찾을 수 없음
+	private static final int EXCEPTION_CODE = 4001;
+
+	public NoSuchImageException(String message) {
+		super(HTTP_STATUS_CODE, EXCEPTION_CODE, message);
+	}
 }
--- domain/mathrank-image-domain/src/main/java/kr/co/mathrank/domain/image/exception/StoreException.java
+++ domain/mathrank-image-domain/src/main/java/kr/co/mathrank/domain/image/exception/StoreException.java
@@ -1,4 +1,12 @@
 package kr.co.mathrank.domain.image.exception;
 
-public class StoreException extends RuntimeException {
+import kr.co.mathrank.common.exception.HttpMathRankException;
+
+public class StoreException extends HttpMathRankException {
+	private static final int HTTP_STATUS_CODE = 504; // 일시적 사용 불가
+	private static final int EXCEPTION_CODE = 4002; // 파일 저장 불가 코드
+
+	public StoreException(String message) {
+		super(HTTP_STATUS_CODE, EXCEPTION_CODE, message);
+	}
 }
