7dbc4a4fd9accf79e6f431af8fa29da140e8d9d7
huhdy32 <mong3268@gmail.com>
2025-07-18 20:56:01 +0900

    feat(oauth): 엔티티에 OAuth 정보 추가

--- domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/entity/Member.java
+++ domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/entity/Member.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import kr.co.mathrank.common.role.Role;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,6 +15,9 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member", indexes = {
+	@Index(name = "idx_oauthUserId", columnList = "oauth_user_id")
+})
 @Getter
 public class Member {
 	@Id
@@ -29,6 +34,9 @@ public class Member {
 	private String password;
 
 	@Embedded
+	private final OAuthInfo oAuthInfo = new OAuthInfo();
+
+	@Embedded
 	private final LockInfo lockInfo = new LockInfo();
 
 	public static Member of(Long id, String name, Role role, String loginId, String password) {
@@ -38,6 +46,18 @@ public class Member {
 		member.name = name;
 		member.loginId = loginId;
 		member.password = password;
+
+		return member;
+	}
+
+	public static Member fromOAuth(final Long id, final Long oAuthId, final OAuthProvider provider,
+		final String nickName, final Role role) {
+		final Member member = new Member();
+		member.id = id;
+		member.oAuthInfo.setOAuthUserId(oAuthId);
+		member.oAuthInfo.setOAuthProvider(provider);
+		member.name = nickName;
+		member.role = role;
 
 		return member;
 	}
