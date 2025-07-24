95655ce992eae1eb0e2ac45cb09c310cfa986a7c
huhdy32 <mong3268@gmail.com>
2025-07-24 10:58:40 +0900

    feat(oauth): oauth 계정 등룍 완료 API 구성

--- app/api/mathrank-auth-api/src/main/java/kr/co/mathrank/app/api/auth/MemberController.java
+++ app/api/mathrank-auth-api/src/main/java/kr/co/mathrank/app/api/auth/MemberController.java
@@ -1,25 +1,32 @@
 package kr.co.mathrank.app.api.auth;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import kr.co.mathrank.domain.auth.dto.MemberInfoResult;
-import kr.co.mathrank.domain.auth.service.MemberQueryService;
+import jakarta.validation.Valid;
+import kr.co.mathrank.app.api.common.authentication.Authorization;
+import kr.co.mathrank.app.api.common.authentication.LoginInfo;
+import kr.co.mathrank.app.api.common.authentication.MemberPrincipal;
+import kr.co.mathrank.domain.auth.dto.MemberInfoCompleteCommand;
+import kr.co.mathrank.domain.auth.service.MemberRegisterService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
-	private final MemberQueryService memberQueryService;
+	private final MemberRegisterService memberRegisterService;
 
-	@Operation(hidden = true)
-	@GetMapping("/api/inner/v1/member/info")
-	public ResponseEntity<MemberInfoResult> getMemberInfo(
-		@RequestParam final Long memberId
+	@Authorization(openedForAll = true)
+	@PutMapping("/api/v1/member/registration")
+	public ResponseEntity<Void> completeRegister(
+		@RequestBody @Valid final MemberRegistrationCompleteRequest request,
+		@LoginInfo final MemberPrincipal memberPrincipal
 	) {
-		return ResponseEntity.ok(memberQueryService.getInfo(memberId));
+		final MemberInfoCompleteCommand command = request.toCommand(memberPrincipal.memberId());
+		memberRegisterService.completeRegister(command);
+
+		return ResponseEntity.ok().build();
 	}
 }
---
+++ app/api/mathrank-auth-api/src/main/java/kr/co/mathrank/app/api/auth/MemberQueryController.java
@@ -0,0 +1,25 @@
+package kr.co.mathrank.app.api.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import kr.co.mathrank.domain.auth.dto.MemberInfoResult;
+import kr.co.mathrank.domain.auth.service.MemberQueryService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberQueryController {
+	private final MemberQueryService memberQueryService;
+
+	@Operation(hidden = true)
+	@GetMapping("/api/inner/v1/member/info")
+	public ResponseEntity<MemberInfoResult> getMemberInfo(
+		@RequestParam final Long memberId
+	) {
+		return ResponseEntity.ok(memberQueryService.getInfo(memberId));
+	}
+}
