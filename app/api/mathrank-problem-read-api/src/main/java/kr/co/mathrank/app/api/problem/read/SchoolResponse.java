c9f311bb39cf075de35c9c9096bf07d5895f230d
huhdy32 <mong3268@gmail.com>
2025-07-28 14:29:51 +0900

    feat(problem): 도로명 주소 리턴하도록 수정

--- app/api/mathrank-problem-read-api/src/main/java/kr/co/mathrank/app/api/problem/read/SchoolResponse.java
+++ app/api/mathrank-problem-read-api/src/main/java/kr/co/mathrank/app/api/problem/read/SchoolResponse.java
@@ -6,13 +6,13 @@ public record SchoolResponse(
 	String schoolCode,
 	String schoolName,
 	String schoolKind,
-	String schoolCity
+	String schoolLocation
 ) {
 	public static SchoolResponse from(final SchoolInfo schoolInfo) {
 		return new SchoolResponse(
 			schoolInfo.SD_SCHUL_CODE(),
 			schoolInfo.SCHUL_NM(),
 			schoolInfo.SCHUL_KND_SC_NM(),
-			schoolInfo.LCTN_SC_NM());
+			schoolInfo.ORG_RDNMA());
 	}
 }
