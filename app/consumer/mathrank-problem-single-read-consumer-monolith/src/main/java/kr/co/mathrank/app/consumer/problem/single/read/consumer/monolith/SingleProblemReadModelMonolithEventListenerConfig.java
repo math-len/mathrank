6199b2d562df8505125de2313b7e2c0a3d08a03d
huhdy32 <mong3268@gmail.com>
2025-08-14 19:42:24 +0900

    refactor: enableasync 위치 수정

--- app/consumer/mathrank-problem-single-read-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/problem/single/read/consumer/monolith/SingleProblemReadModelMonolithEventListenerConfig.java
+++ app/consumer/mathrank-problem-single-read-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/problem/single/read/consumer/monolith/SingleProblemReadModelMonolithEventListenerConfig.java
@@ -5,7 +5,9 @@ import java.util.concurrent.Executors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @Configuration
 public class SingleProblemReadModelMonolithEventListenerConfig {
 	@Bean
