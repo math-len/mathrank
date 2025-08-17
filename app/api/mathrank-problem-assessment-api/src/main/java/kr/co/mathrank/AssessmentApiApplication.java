31ced53890ec877e73f60fcd92024b42bf7ea505
huhdy32 <mong3268@gmail.com>
2025-08-17 10:28:24 +0900

    fix(assessment): 어플리케이션 실행 시, args 전하도록 변경

--- app/api/mathrank-problem-assessment-api/src/main/java/kr/co/mathrank/AssessmentApiApplication.java
+++ app/api/mathrank-problem-assessment-api/src/main/java/kr/co/mathrank/AssessmentApiApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AssessmentApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AssessmentApiApplication.class);
+		SpringApplication.run(AssessmentApiApplication.class, args);
 	}
 }
