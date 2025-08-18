ce21e215f22d39a801e592610445daac9897815c
huhdy32 <mong3268@gmail.com>
2025-08-18 20:26:39 +0900

    refactor(assessment): 클래스 레벨 접근 제어자 수정

--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/constraint/ScoreSumConstraintValidator.java
+++ domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/constraint/ScoreSumConstraintValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import kr.co.mathrank.domain.problem.assessment.dto.AssessmentItemRegisterCommand;
 
-public class ScoreSumConstraintValidator implements ConstraintValidator<ScoreSum, List<AssessmentItemRegisterCommand>> {
+class ScoreSumConstraintValidator implements ConstraintValidator<ScoreSum, List<AssessmentItemRegisterCommand>> {
 	private static final int SCORE_SUM = 100; // 점수의 총 합 변경지점 최소화를 위해 상수로 설정
 
 	@Override
