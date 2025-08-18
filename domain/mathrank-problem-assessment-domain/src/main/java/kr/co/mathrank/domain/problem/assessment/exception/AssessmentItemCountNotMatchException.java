fe9669c5b25e05197c2f85e6cc64dc7093cb71a3
huhdy32 <mong3268@gmail.com>
2025-08-18 20:15:56 +0900

    refactor(assessment): 사용되지 않는 예외 제거

--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/exception/AssessmentItemCountNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package kr.co.mathrank.domain.problem.assessment.exception;
-
-public class AssessmentItemCountNotMatchException extends AssessmentException {
-	public AssessmentItemCountNotMatchException() {
-		super(7003, "문제 갯수와 점수갯수가 일치하지 않음");
-	}
-}
--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/exception/AssessmentItemTotalScoreException.java
+++
@@ -1,7 +0,0 @@
-package kr.co.mathrank.domain.problem.assessment.exception;
-
-public class AssessmentItemTotalScoreException extends AssessmentException {
-	public AssessmentItemTotalScoreException() {
-		super(7002, "문제집의 점수 총 합이 100이 아닙니다.");
-	}
-}
