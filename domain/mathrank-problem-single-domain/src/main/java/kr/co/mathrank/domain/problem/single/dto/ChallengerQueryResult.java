210edf821e91f2dcd44c114a1945b692ba497426
huhdy32 <mong3268@gmail.com>
2025-08-21 11:32:01 +0900

    fix(single-problem-query): 사용되지 않는 메소드 삭제

--- domain/mathrank-problem-single-domain/src/main/java/kr/co/mathrank/domain/problem/single/dto/ChallengerQueryResult.java
+++ domain/mathrank-problem-single-domain/src/main/java/kr/co/mathrank/domain/problem/single/dto/ChallengerQueryResult.java
@@ -1,13 +1,7 @@
 package kr.co.mathrank.domain.problem.single.dto;
 
-import kr.co.mathrank.domain.problem.single.entity.Challenger;
-
 public record ChallengerQueryResult(
 	Long singleProblemId,
 	Boolean success
 ) {
-	public static ChallengerQueryResult from(final Challenger challenger) {
-		return new ChallengerQueryResult(challenger.getSingleProblem().getProblemId(),
-			challenger.isSuccessAtFirstTry());
-	}
 }
