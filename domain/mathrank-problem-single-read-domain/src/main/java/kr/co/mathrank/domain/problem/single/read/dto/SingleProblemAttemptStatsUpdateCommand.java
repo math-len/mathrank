856ddfe3f2a12081cbfa655bd9a1ba132bb2a6be
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-08-21 15:00:25 +0900

    feat(single-problem-read): 개별문제 조회, 결과에 풀이 여부 포함되도록 구현 (#72)
    * feat(single-problem-read): 문제 풀이 이벤트 수신 시, 해당 사용자의 풀이 기록이 없으면 새로 삽입
    
    * feat(single-problem-read): 페이지 결과에 해당 사용자의 풀이 여부 포함하도록 수정
    
    * fix: 쿼리 수정
    
    * fix: 유니크 인덱스 수정
    
    * refactor(single-problem-solve): 오버 플로우 방지하기 위해 메모리 전체 로드 대신, 개별 쿼리로 수정
    
    * refactor(single-problem-solve): 오버 플로우 방지를 위해, IN 절을 이용해 하나의 쿼리로 처리
    
    * refactor(single-problem-solve): 사용하지 않는 메소드 제거
    
    * test(single-problem-query): 테스트 케이스 에서 오류 수정
    
    * refactor(single-problem-solve): 의미 없는 사용임에 따라 set 에서 list로 변경
    
    * refactor(single-problem-solve): toString exclude 추가
    
    * refactor(single-problem-read): consumer 페이로드 업데이트
    
    * refactor(single-problem-read): monolith consumer 페이로드 업데이트

--- domain/mathrank-problem-single-read-domain/src/main/java/kr/co/mathrank/domain/problem/single/read/dto/SingleProblemAttemptStatsUpdateCommand.java
+++ domain/mathrank-problem-single-read-domain/src/main/java/kr/co/mathrank/domain/problem/single/read/dto/SingleProblemAttemptStatsUpdateCommand.java
@@ -1,12 +1,14 @@
 package kr.co.mathrank.domain.problem.single.read.dto;
 
-import java.time.LocalDateTime;
-
 import jakarta.validation.constraints.NotNull;
 
 public record SingleProblemAttemptStatsUpdateCommand(
 	@NotNull
 	Long singleProblemId, // singleProblemId is different from problemId
+	@NotNull
+	Long memberId,
+	@NotNull
+	Boolean success,
 	@NotNull
 	Long firstTrySuccessCount,
 	@NotNull
---
+++ domain/mathrank-problem-single-read-domain/src/main/java/kr/co/mathrank/domain/problem/single/read/entity/SingleProblemSolver.java
@@ -0,0 +1,46 @@
+package kr.co.mathrank.domain.problem.single.read.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(
+	uniqueConstraints = @UniqueConstraint(
+		name = "unique_memberId_singleProblemId",
+		columnNames = {"member_id", "single_problem_read_model_id"}
+	)
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "singleProblemReadModel")
+public class SingleProblemSolver {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long memberId;
+
+	private boolean success; // success or fail 둘 중 하나
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private SingleProblemReadModel singleProblemReadModel;
+
+	public static SingleProblemSolver of(final SingleProblemReadModel singleProblemReadModel, final Long memberId, final boolean success) {
+		final SingleProblemSolver singleProblemSolver = new SingleProblemSolver();
+		singleProblemSolver.memberId = memberId;
+		singleProblemSolver.singleProblemReadModel =singleProblemReadModel;
+		singleProblemSolver.success = success;
+
+		return singleProblemSolver;
+	}
+}
