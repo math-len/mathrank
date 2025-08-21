0e120bcdcbef41dd3a9a23ce013dfeff6d19e4a5
huhdy32 <mong3268@gmail.com>
2025-08-21 14:39:55 +0900

    refactor(single-problem-read): consumer 페이로드 업데이트

--- app/consumer/mathrank-problem-single-consumer/src/main/java/kr/co/mathrank/app/consumer/problem/single/SingleProblemStatisticsUpdatedPayload.java
+++ app/consumer/mathrank-problem-single-consumer/src/main/java/kr/co/mathrank/app/consumer/problem/single/SingleProblemStatisticsUpdatedPayload.java
@@ -5,6 +5,9 @@ import kr.co.mathrank.domain.problem.single.read.dto.SingleProblemAttemptStatsUp
 
 public record SingleProblemStatisticsUpdatedPayload(
 	Long singleProblemId,
+	Long problemId,
+	Long memberId,
+	Boolean success,
 	Long firstTrySuccessCount,
 	Long totalAttemptedCount,
 	Long attemptedUserDistinctCount
@@ -12,6 +15,8 @@ public record SingleProblemStatisticsUpdatedPayload(
 	public SingleProblemAttemptStatsUpdateCommand toCommand() {
 		return new SingleProblemAttemptStatsUpdateCommand(
 			this.singleProblemId,
+			memberId,
+			success,
 			this.firstTrySuccessCount,
 			this.totalAttemptedCount,
 			this.attemptedUserDistinctCount
