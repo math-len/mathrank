32ad602c4906590ce4c070b0caa73846ec2a1852
huhdy32 <mong3268@gmail.com>
2025-08-22 17:25:45 +0900

    feat(cache): 카페인 캐시 적용 모듈 구현

---
+++ common/mathrank-cache-caffeine/src/main/java/kr/co/mathrank/common/cache/manager/CaffeineCacheCustomizerConfiguration.java
@@ -0,0 +1,37 @@
+package kr.co.mathrank.common.cache.manager;
+
+import java.util.List;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import kr.co.mathrank.common.cache.RequiredCacheSpec;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableCaching
+@Configuration
+public class CaffeineCacheCustomizerConfiguration {
+	@Bean
+	public CacheManager applyRequireCacheSpecs(final List<RequiredCacheSpec> requireCacheSpecs) {
+		final CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		for (final RequiredCacheSpec cacheSpec : requireCacheSpecs) {
+			final Cache<Object, Object> cache = Caffeine.newBuilder()
+				.maximumSize(200L)  // 엔트리 최대 200개
+				.expireAfterWrite(cacheSpec.ttl()).build();
+			cacheManager.registerCustomCache(cacheSpec.cacheName(), cache);
+
+			log.info(
+				"[CaffeineCacheCustomizerConfiguration.applyRequireCacheSpecs]: registered cache spec - request module: {}, cache name: {}, cache TTL: {}",
+				cacheSpec.moduleName(), cacheSpec.cacheName(), cacheSpec.ttl());
+		}
+
+		return cacheManager;
+	}
+}
