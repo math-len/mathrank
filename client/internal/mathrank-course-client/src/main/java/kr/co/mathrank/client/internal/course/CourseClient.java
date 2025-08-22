d4e166f70eb0bf4a08656cfb5bbb25dfb0a0e788
huhdy32 <mong3268@gmail.com>
2025-08-22 12:34:25 +0900

    fix(problem-read): properties 클래스 필드 직접 호출로 인한 에러 해결

--- client/internal/mathrank-course-client/src/main/java/kr/co/mathrank/client/internal/course/CourseClient.java
+++ client/internal/mathrank-course-client/src/main/java/kr/co/mathrank/client/internal/course/CourseClient.java
@@ -10,7 +10,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class CourseClient {
 	private static final String URL_FORMAT = "%s:%s";
@@ -18,12 +21,16 @@ public class CourseClient {
 	private final RestClient restClient;
 
 	CourseClient(final CourseClientProperties properties) {
+		final String baseURL = URL_FORMAT.formatted(properties.getHost(), properties.getPort());
+		log.info("[CourseClient.new] init : {}", properties);
+		log.info("[CourseClient.new] init : {}", baseURL);
 		this.restClient = RestClient.builder()
-			.baseUrl(getUrlFormat(properties.host, properties.port))
+			.baseUrl(baseURL)
 			.build();
 	}
 
 	public CourseQueryContainsParentsResult getParentCourses(final String coursePath) {
+		log.info("[CourseClient.getParentCourses]: called - coursePath: {}", coursePath);
 		return restClient.get()
 			.uri(uriBuilder -> uriBuilder
 				.path("/api/inner/v1/course/parents")
@@ -43,6 +50,7 @@ public class CourseClient {
 	@NoArgsConstructor
 	@Setter
 	@Validated
+	@ToString
 	static class CourseClientProperties {
 		@NotNull
 		private String host;
