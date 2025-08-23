76d485814c906552b2a2507f891484739289710a
huhdy32 <mong3268@gmail.com>
2025-08-23 22:07:15 +0900

    refactor(assessment): configuration 빈으로 등록

--- app/consumer/mathrank-assessment-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/problem/assessment/AssessmentMonolithEventConsumerConfig.java
+++ app/consumer/mathrank-assessment-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/problem/assessment/AssessmentMonolithEventConsumerConfig.java
@@ -3,12 +3,12 @@ package kr.co.mathrank.app.consumer.problem.assessment;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @EnableAsync
 class AssessmentMonolithEventConsumerConfig {
 	@Bean
