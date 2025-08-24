96673418f82582f07f5f342d0dcd6e1c6e62d0d3
huhdy32 <mong3268@gmail.com>
2025-08-24 13:42:52 +0900

    style: import 정리

--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/exception/AssessmentRegisterException.java
+++ domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/exception/AssessmentRegisterException.java
@@ -1,7 +1,5 @@
 package kr.co.mathrank.domain.problem.assessment.exception;
 
-import kr.co.mathrank.domain.problem.assessment.entity.Assessment;
-
 public class AssessmentRegisterException extends AssessmentException {
 	public AssessmentRegisterException() {
 		super(7001, "등록할 수 있는 권한 부재 (관리자만 등록 가능)");
