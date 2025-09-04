00f8e05fac7698e94caa5dbeb7997439acb503c8
huhdy32 <mong3268@gmail.com>
2025-09-04 10:19:09 +0900

    feat(exam): 기간제 문제집 command api 구성

---
+++ app/api/mathrank-problem-exam-api/src/main/java/kr/co/mathrank/app/api/problem/exam/ExamController.java
@@ -0,0 +1,52 @@
+package kr.co.mathrank.app.api.problem.exam;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import kr.co.mathrank.app.api.common.authentication.Authorization;
+import kr.co.mathrank.app.api.common.authentication.LoginInfo;
+import kr.co.mathrank.app.api.common.authentication.MemberPrincipal;
+import kr.co.mathrank.common.role.Role;
+import kr.co.mathrank.domain.problem.assessment.dto.LimitedAssessmentRegisterCommand;
+import kr.co.mathrank.domain.problem.assessment.dto.SubmissionRegisterCommand;
+import kr.co.mathrank.domain.problem.assessment.service.AssessmentRegisterService;
+import kr.co.mathrank.domain.problem.assessment.service.SubmissionRegisterService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "시험지 API")
+public class ExamController {
+	private final AssessmentRegisterService assessmentRegisterService;
+	private final SubmissionRegisterService submissionRegisterService;
+
+	@Operation(summary = "시험지 등록", description = "문제집 등록은 관리자만 가능합니다.")
+	@PostMapping("/api/v1/problem/exam")
+	@Authorization(values = Role.ADMIN)
+	public ResponseEntity<Void> registerAssessment(
+		@RequestBody @Valid final Requests.ExamRegisterRequest request,
+		@LoginInfo final MemberPrincipal memberPrincipal
+	) {
+		final LimitedAssessmentRegisterCommand command = request.toCommand(memberPrincipal.memberId(), memberPrincipal.role());
+		assessmentRegisterService.registerLimited(command);
+
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@Operation(summary = "시험지 답안지 등록 API")
+	@PostMapping("/api/v1/problem/exam/submission")
+	@Authorization(openedForAll = true)
+	public ResponseEntity<Long> registerSubmission(
+		@RequestBody @Valid final Requests.ExamSubmissionRegisterRequest request,
+		@LoginInfo final MemberPrincipal memberPrincipal
+	) {
+		final SubmissionRegisterCommand command = request.toCommand(memberPrincipal.memberId());
+		return ResponseEntity.status(HttpStatus.CREATED).body(submissionRegisterService.submit(command));
+	}
+}
