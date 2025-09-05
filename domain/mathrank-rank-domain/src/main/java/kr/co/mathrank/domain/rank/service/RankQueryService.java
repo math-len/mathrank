1f2c6e6d59f4d82a8889cf9e946dc08f9c34d343
huhdy32 <mong3268@gmail.com>
2025-09-05 14:18:17 +0900

    refactor(rank): 점수 조회 로직 추상화 제거

--- domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/RankQueryService.java
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/RankQueryService.java
@@ -14,13 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 public class RankQueryService {
 	private final SolverRepository solverRepository;
 
-	private final RankScoreQueryManager rankScoreQueryManager;
-
 	public RankQueryResult getRank(final Long memberId) {
 		final long rank = solverRepository.findRankByMemberId(memberId) + 1;
 		final long totalMemberCount = solverRepository.count();
 		final Tier tier = Tier.getMatchTier(rank, totalMemberCount);
-		final Long score = rankScoreQueryManager.getScore(memberId);
+		final Long score = solverRepository.findScoreByMemberId(memberId);
 
 		return RankQueryResult.of(rank, tier, score, totalMemberCount);
 	}
