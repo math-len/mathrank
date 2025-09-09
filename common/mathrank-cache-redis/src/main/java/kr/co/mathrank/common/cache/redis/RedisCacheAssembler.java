78cb93c167134d9fb7c7876c2b572ccf77249667
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-09-09 21:33:51 +0900

    feat(cache-redis): 레디스 캐시 어셈블러 구성 (#140)
    * feat(cache-redis): 레디스 캐시 어셈블러 구성
    
    * refactor(cache-redis): 연결 확인 로직 제거
    
    * refactor: 불필요한 어노테이션 제거
    
    * refactor(cache-redis): 빈 등록으로 중복 호출 제거
    
    * refactor(cache-redis): 테스트 명 변경
    
    * refactor(cache-redis): 테스트 클래스의 캐시 키 명 변경
    
    * refactor(cache-redis): 테스트에서 try-catch 제거

---
+++ common/mathrank-cache-redis/src/main/java/kr/co/mathrank/common/cache/redis/RedisCacheAssembler.java
@@ -0,0 +1,71 @@
+package kr.co.mathrank.common.cache.redis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import kr.co.mathrank.common.cache.CacheSpecAssembler;
+import kr.co.mathrank.common.cache.RequiredCacheSpec;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class RedisCacheAssembler implements CacheSpecAssembler {
+	private final RedisConnectionFactory redisConnectionFactory;
+
+	@Bean
+	@Override
+	public CacheManager assemble(List<RequiredCacheSpec> requiredCacheSpecs) {
+		final Map<String, RedisCacheConfiguration> cacheEntryConfigurations = mapToRedisCacheConfigurations(requiredCacheSpecs);
+		return createCacheManager(cacheEntryConfigurations);
+	}
+
+	private CacheManager createCacheManager(
+		Map<String, RedisCacheConfiguration> caches
+	) {
+		return RedisCacheManager.builder()
+			.withInitialCacheConfigurations(caches)
+			.cacheWriter(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+			.build();
+	}
+
+	@Bean
+	public RedisCacheConfiguration initSerializeConfiguration() {
+		return RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()
+					.configure(objectMapper -> objectMapper.registerModule(new JavaTimeModule()))));
+	}
+
+	/**
+	 * {@link RequiredCacheSpec}을 {@link RedisCacheConfiguration} 로 변환해주는 메소드
+	 * @param requiredCacheSpecs
+	 * @return
+	 */
+	private Map<String, RedisCacheConfiguration> mapToRedisCacheConfigurations(
+		final List<RequiredCacheSpec> requiredCacheSpecs
+	) {
+		return requiredCacheSpecs.stream()
+			.collect(Collectors.toMap(
+				RequiredCacheSpec::cacheName,
+				requiredCacheSpec -> initSerializeConfiguration()
+					.entryTtl(requiredCacheSpec.ttl())));
+	}
+}
---
+++ common/mathrank-cache-redis/src/test/java/kr/co/mathrank/CommonRedisCacheConfigurationTestApplication.java
@@ -0,0 +1,9 @@
+package kr.co.mathrank;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CommonRedisCacheConfigurationTestApplication {
+}
