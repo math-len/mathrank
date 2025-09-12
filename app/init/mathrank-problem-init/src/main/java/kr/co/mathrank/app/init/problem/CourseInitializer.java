c61ee34f6feade1cfbb95e589c0ff0b9378d6d32
huhdy32 <mong3268@gmail.com>
2025-09-12 10:58:34 +0900

    chore: s3 업로드 액션

--- app/init/mathrank-problem-init/src/main/java/kr/co/mathrank/app/init/problem/CourseInitializer.java
+++ app/init/mathrank-problem-init/src/main/java/kr/co/mathrank/app/init/problem/CourseInitializer.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
+// @Component
 @RequiredArgsConstructor
 public class CourseInitializer implements CommandLineRunner {
 	private final CourseRepository courseRepository;
