da39defad7da7742be67af11a834eec7c58ff2f2
huhdy32 <mong3268@gmail.com>
2025-09-15 19:08:33 +0900

    feat(oauth-google): 구글 oAuth 클라이언트 추가

---
+++ domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/client/GoogleOAuthClient.java
@@ -0,0 +1,57 @@
+package kr.co.mathrank.domain.auth.client;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import kr.co.mathrank.domain.auth.dto.OAuthLoginCommand;
+import kr.co.mathrank.domain.auth.entity.OAuthProvider;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+class GoogleOAuthClient implements OAuthClientHandler {
+	private static final String TOKEN_FORMAT = "Bearer %s";
+	private final GoogleConfiguration googleConfiguration;
+
+	private final RestClient tokenClient = RestClient.builder()
+		.baseUrl("https://oauth2.googleapis.com/token")
+		.build();
+
+	private final RestClient infoClient = RestClient.builder()
+		.baseUrl("https://www.googleapis.com/oauth2/v2/userinfo")
+		.build();
+
+	@Override
+	public MemberInfoResponse getMemberInfo(OAuthLoginCommand command) {
+		return getInfo(getAccessToken(command).access_token());
+	}
+
+	private GoogleInfoResponse getInfo(final String accessToken) {
+		return infoClient.post()
+			.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+			.header(HttpHeaders.AUTHORIZATION, TOKEN_FORMAT.formatted(accessToken))
+			.retrieve()
+			.body(GoogleInfoResponse.class);
+	}
+
+	private AccessTokenResponse getAccessToken(final OAuthLoginCommand command) {
+		return tokenClient.post()
+			.uri(uriBuilder -> uriBuilder
+				.queryParam("client_id", googleConfiguration.getClientId())
+				.queryParam("client_secret", googleConfiguration.getClientSecret())
+				.queryParam("code", command.code())
+				.queryParam("grant_type", "authorization_code")
+				.queryParam("redirect_uri", googleConfiguration.getRedirectUri())
+				.build())
+			.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+			.retrieve()
+			.body(AccessTokenResponse.class);
+	}
+
+	@Override
+	public boolean supports(OAuthProvider provider) {
+		return false;
+	}
+}
