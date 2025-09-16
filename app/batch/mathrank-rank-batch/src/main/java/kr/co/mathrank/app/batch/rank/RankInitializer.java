6e51913b39db2e7d84713c3d53293d5ba98ca605
huhdy32 <mong3268@gmail.com>
2025-09-16 14:48:10 +0900

    feat(rank): 컨텍스트 준비되면 실행되도록 수정

--- app/batch/mathrank-rank-batch/src/main/java/kr/co/mathrank/app/batch/rank/RankInitializer.java
+++ app/batch/mathrank-rank-batch/src/main/java/kr/co/mathrank/app/batch/rank/RankInitializer.java
@@ -1,9 +1,7 @@
 package kr.co.mathrank.app.batch.rank;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import kr.co.mathrank.domain.rank.service.RankFetchService;
@@ -13,11 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@EnableScheduling
 class RankInitializer {
 	private final RankFetchService rankFetchService;
 
-	@Scheduled(initialDelay = 5, timeUnit = TimeUnit.SECONDS)
+	@EventListener(ApplicationReadyEvent.class)
 	void initializeRankInfoToRedis() {
 		rankFetchService.syncRedis();
 		log.info("[RankInitializer.initializeRankInfoToRedis] rank initialization complete.");
