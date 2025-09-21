5ec87058b1683d6827a83cfb221947807b65a6ef
huhdy32 <mong3268@gmail.com>
2025-09-21 20:00:28 +0900

    refactor(board): 접근 제어자 수정

--- domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/repository/PostQueryRepository.java
+++ domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/repository/PostQueryRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import kr.co.mathrank.domain.board.dto.PostPageQuery;
 import kr.co.mathrank.domain.board.entity.Post;
 
-public interface PostQueryRepository {
+interface PostQueryRepository {
 	List<Post> findAllByPage(
 		PostPageQuery postQuery,
 		int pageSize,
