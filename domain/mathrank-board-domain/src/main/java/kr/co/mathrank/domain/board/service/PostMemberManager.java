974ae969792d8bd3258a8c9c353560e58e7ee0e8
huhdy32 <mong3268@gmail.com>
2025-09-21 23:32:18 +0900

    fix(board): validated 추가

--- domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/service/PostMemberManager.java
+++ domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/service/PostMemberManager.java
@@ -1,12 +1,14 @@
 package kr.co.mathrank.domain.board.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.constraints.NotNull;
 import kr.co.mathrank.client.internal.member.MemberClient;
 import lombok.RequiredArgsConstructor;
 
 @Component
+@Validated
 @RequiredArgsConstructor
 class PostMemberManager {
 	private final MemberClient memberClient;
