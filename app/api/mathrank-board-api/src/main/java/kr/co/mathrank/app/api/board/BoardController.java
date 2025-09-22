b33f6c58ca79bb69f91ccabb1e93f1d75ae6f3f6
huhdy32 <mong3268@gmail.com>
2025-09-22 15:36:42 +0900

    feat(board): 게시글 삭제 API 구성

--- app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/BoardController.java
+++ app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/BoardController.java
@@ -2,9 +2,12 @@ package kr.co.mathrank.app.api.board;
 
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,8 +16,10 @@ import jakarta.validation.Valid;
 import kr.co.mathrank.app.api.common.authentication.Authorization;
 import kr.co.mathrank.app.api.common.authentication.LoginInfo;
 import kr.co.mathrank.app.api.common.authentication.MemberPrincipal;
+import kr.co.mathrank.domain.board.dto.PostDeleteCommand;
 import kr.co.mathrank.domain.board.dto.PostRegisterCommand;
 import kr.co.mathrank.domain.board.dto.PostUpdateCommand;
+import kr.co.mathrank.domain.board.service.PostDeleteService;
 import kr.co.mathrank.domain.board.service.PostRegisterService;
 import kr.co.mathrank.domain.board.service.PostUpdateService;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 	private final PostRegisterService postRegisterService;
 	private final PostUpdateService postUpdateService;
+	private final PostDeleteService postDeleteService;
 
 	@Operation(summary = "게시글 등록 API")
 	@PostMapping("/api/v1/board/post")
@@ -52,4 +58,16 @@ public class BoardController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "게시글 삭제 API")
+	@DeleteMapping("/api/v1/board/post/{postId}")
+	@Authorization(openedForAll = true)
+	public ResponseEntity<Void> delete(
+		@PathVariable final Long postId,
+		@LoginInfo final MemberPrincipal memberPrincipal
+	) {
+		final PostDeleteCommand command = new PostDeleteCommand(postId, memberPrincipal.memberId(),
+			memberPrincipal.role());
+		postDeleteService.delete(command);
+		return ResponseEntity.ok().build();
+	}
 }
