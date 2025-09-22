1313db6ed56fc0f9a70e13089255c545edf78639
huhdy32 <mong3268@gmail.com>
2025-09-22 16:34:30 +0900

    feat(board): 게시글 페이지 조회 API 구성

--- app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/BoardController.java
+++ app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/BoardController.java
@@ -1,8 +1,10 @@
 package kr.co.mathrank.app.api.board;
 
+import org.hibernate.validator.constraints.Range;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,14 @@ import jakarta.validation.Valid;
 import kr.co.mathrank.app.api.common.authentication.Authorization;
 import kr.co.mathrank.app.api.common.authentication.LoginInfo;
 import kr.co.mathrank.app.api.common.authentication.MemberPrincipal;
+import kr.co.mathrank.common.page.PageResult;
 import kr.co.mathrank.domain.board.dto.PostDeleteCommand;
+import kr.co.mathrank.domain.board.dto.PostPageQuery;
+import kr.co.mathrank.domain.board.dto.PostPageQueryResult;
 import kr.co.mathrank.domain.board.dto.PostRegisterCommand;
 import kr.co.mathrank.domain.board.dto.PostUpdateCommand;
 import kr.co.mathrank.domain.board.service.PostDeleteService;
+import kr.co.mathrank.domain.board.service.PostQueryService;
 import kr.co.mathrank.domain.board.service.PostRegisterService;
 import kr.co.mathrank.domain.board.service.PostUpdateService;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +37,7 @@ public class BoardController {
 	private final PostRegisterService postRegisterService;
 	private final PostUpdateService postUpdateService;
 	private final PostDeleteService postDeleteService;
+	private final PostQueryService postQueryService;
 
 	@Operation(summary = "게시글 등록 API")
 	@PostMapping("/api/v1/board/post")
@@ -69,5 +76,15 @@ public class BoardController {
 			memberPrincipal.role());
 		postDeleteService.delete(command);
 		return ResponseEntity.ok().build();
+	}
+
+	@Operation(summary = "페이지 조회 API", description = "정렬은 날짜 내림차순을로 적용됩니다.")
+	@GetMapping("/api/v1/board/post")
+	public ResponseEntity<PageResult<PostPageQueryResult>> pageQuery(
+		@RequestParam(defaultValue = "1") @Range(min = 1, max = 1000) final Integer pageNumber,
+		@RequestParam(defaultValue = "20") @Range(min = 1, max = 20) final Integer pageSize,
+		@ModelAttribute @ParameterObject final PostPageQuery query
+	) {
+		return ResponseEntity.ok(postQueryService.pageQuery(query, pageSize, pageNumber));
 	}
 }
