121f42a7dbe085937783192683e5c255e7ac989f
huhdy32 <mong3268@gmail.com>
2025-09-23 12:52:28 +0900

    refactor(board): 접근제어 수정

--- domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/entity/Comment.java
+++ domain/mathrank-board-domain/src/main/java/kr/co/mathrank/domain/board/entity/Comment.java
@@ -10,10 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
