fb6fe9c6bf5023efdbaec555436bd61b3a537af9
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-09-23 18:07:27 +0900

    hotfix(rank): 풀이 기록이 없을 시 발생하던 500 에러 수정 (#193)
    변경 전: 점수 없을 시, nullPointerException 으로 인한 500 에러
    
    변경 후: 점수 없을 시, 예외 처리 하도록 수정

--- domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/RankQueryService.java
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/RankQueryService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import kr.co.mathrank.domain.rank.dto.RankQueryResult;
 import kr.co.mathrank.domain.rank.entity.Tier;
+import kr.co.mathrank.domain.rank.exception.CannotCalculateRankException;
 import kr.co.mathrank.domain.rank.repository.RankRepository;
 import kr.co.mathrank.domain.rank.repository.SolverRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +17,16 @@ public class RankQueryService {
 	private final RankRepository rankRepository;
 
 	public RankQueryResult getRank(final Long memberId) {
+		final Long score = rankRepository.getScore(String.valueOf(memberId));
+
+		if (score == null) {
+			log.info("[RankQueryService.getRank] cannot find score - memberId: {}", memberId);
+			throw new CannotCalculateRankException("점수 기록이 없어 랭크를 계산할 수 없습니다.");
+		}
+
 		final long rank = rankRepository.getRank(String.valueOf(memberId));
 		final long totalMemberCount = rankRepository.getTotalRankerCount();
 		final Tier tier = Tier.getMatchTier(rank, totalMemberCount);
-		final Long score = rankRepository.getScore(String.valueOf(memberId));
 
 		return RankQueryResult.of(rank, tier, score, totalMemberCount);
 	}
