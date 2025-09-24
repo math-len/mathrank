b75a0aae33e607778d4b132a2b32eb5012a775e6
huhdy32 <mong3268@gmail.com>
2025-09-24 18:41:43 +0900

    hotfix(board): id 타입 long -> string 변경

--- app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/Responses.java
+++ app/api/mathrank-board-api/src/main/java/kr/co/mathrank/app/api/board/Responses.java
@@ -6,46 +6,47 @@ import java.util.List;
 import kr.co.mathrank.client.internal.member.MemberInfo;
 import kr.co.mathrank.domain.board.dto.CommentDetailResult;
 import kr.co.mathrank.domain.board.dto.PostDetailQueryResult;
+import kr.co.mathrank.domain.board.dto.PostPageQueryResult;
 import kr.co.mathrank.domain.board.entity.PostType;
 
 public class Responses {
 	record PostDetailResponse(
-		Long postId,
+		String postId,
 		MemberResponse memberInfo,
 		PostType postType,
 		String title,
 		String content,
 		List<CommentDetailResponse> comments,
-		Long singleProblemId,
-		Long assessmentId,
-		Long contestId,
+		String singleProblemId,
+		String assessmentId,
+		String contestId,
 		LocalDateTime createdAt
 	) {
 		public static PostDetailResponse from(final PostDetailQueryResult postResult, final List<CommentDetailResponse> comments) {
 			return new PostDetailResponse(
-				postResult.postId(),
+				String.valueOf(postResult.postId()),
 				MemberResponse.from(postResult.memberId(), postResult.memberNickName()),
 				postResult.postType(),
 				postResult.title(),
 				postResult.content(),
 				comments,
-				postResult.singleProblemId(),
-				postResult.assessmentId(),
-				postResult.contestId(),
+				String.valueOf(postResult.singleProblemId()),
+				String.valueOf(postResult.assessmentId()),
+				String.valueOf(postResult.contestId()),
 				postResult.createdAt()
 			);
 		}
 	}
 
 	record CommentDetailResponse(
-		Long commentId,
+		String commentId,
 		MemberResponse memberInfo,
 		String content,
 		LocalDateTime createdAt
 	) {
 		public static CommentDetailResponse from(final CommentDetailResult commentDetailResult, final MemberInfo memberInfo) {
 			return new CommentDetailResponse(
-				commentDetailResult.commentId(),
+				String.valueOf(commentDetailResult.commentId()),
 				MemberResponse.from(memberInfo),
 				commentDetailResult.content(),
 				commentDetailResult.createdAt()
@@ -54,12 +55,12 @@ public class Responses {
 	}
 
 	record MemberResponse(
-		Long memberId,
+		String memberId,
 		String nickName
 	) {
 		public static MemberResponse from(final MemberInfo memberInfo) {
 			return new MemberResponse(
-				memberInfo.memberId(),
+				String.valueOf(memberInfo.memberId()),
 				memberInfo.memberName()
 			);
 		}
@@ -67,8 +68,40 @@ public class Responses {
 		// Post의 경우, 닉네임 기반 조회라 client를 통한 닉네임 조회 필요 없음
 		public static MemberResponse from(final Long memberId, final String nickName) {
 			return new MemberResponse(
-				memberId,
+				String.valueOf(memberId),
 				nickName
+			);
+		}
+	}
+
+	record PostPageQueryResponse(
+		String postId,
+		String memberId,
+		String memberNickName,
+		PostType postType,
+
+		String title,
+
+		String singleProblemId,
+		String assessmentId,
+		String contestId,
+
+		Integer commentCount,
+
+		LocalDateTime createdAt
+	) {
+		public static PostPageQueryResponse from(final PostPageQueryResult result) {
+			return new PostPageQueryResponse(
+				String.valueOf(result.postId()),
+				String.valueOf(result.memberId()),
+				result.memberNickName(),
+				result.postType(),
+				result.title(),
+				String.valueOf(result.singleProblemId()),
+				String.valueOf(result.assessmentId()),
+				String.valueOf(result.contestId()),
+				result.commentCount(),
+				result.createdAt()
 			);
 		}
 	}
