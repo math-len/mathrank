3cddbfe8dd7d5f49a63e7c7bf4b611b965386a6b
huhdy32 <mong3268@gmail.com>
2025-10-02 22:54:06 +0900

    fix(single-problem): validation 추가

--- domain/mathrank-problem-single-domain/src/main/java/kr/co/mathrank/domain/problem/single/dto/SingleProblemDeleteCommand.java
+++ domain/mathrank-problem-single-domain/src/main/java/kr/co/mathrank/domain/problem/single/dto/SingleProblemDeleteCommand.java
@@ -1,6 +1,9 @@
 package kr.co.mathrank.domain.problem.single.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record SingleProblemDeleteCommand(
+	@NotNull
 	Long singleProblemId
 ) {
 }
