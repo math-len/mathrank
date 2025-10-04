50f8d54b55d415b7f25c1b2713968e690f0172a1
huhdy32 <mong3268@gmail.com>
2025-10-04 12:26:42 +0900

    feat(problem): 문제 삭제 시, 이벤트 발행

---
+++ domain/mathrank-point-domain/src/main/java/kr/co/mathrank/domain/point/service/PointPurchaseService.java
@@ -0,0 +1,13 @@
+package kr.co.mathrank.domain.point.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Validated
+@RequiredArgsConstructor
+public class PointPurchaseService {
+	private final
+}
