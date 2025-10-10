5805a6d3dd81ab128d47914bd637653047351619
huhdy32 <mong3268@gmail.com>
2025-10-10 16:09:25 +0900

    feat(file): 파일 다운로드 API 구성

--- app/api/mathrank-image-api/src/main/java/kr/co/mathrank/app/api/image/ImageController.java
+++ app/api/mathrank-image-api/src/main/java/kr/co/mathrank/app/api/image/ImageController.java
@@ -1,7 +1,13 @@
 package kr.co.mathrank.app.api.image;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,6 +43,18 @@ public class ImageController {
 			.body(media.data().readAllBytes());
 	}
 
+	@Operation(summary = "파일 다운로드 API", description = "fileRealName 에 지정한 이름으로 다운로드 됩니다. ")
+	@GetMapping("/api/v1/file/download")
+	public ResponseEntity<InputStreamResource> download(
+		@RequestParam final String fileSource,
+		@RequestParam final String fileRealName
+	) {
+		final ImageFileResult result = imageService.load(fileSource);
+		final HttpHeaders headers = getDownloadHeaders(fileRealName);
+
+		return new ResponseEntity<>(new InputStreamResource(result.data()) , headers, HttpStatus.OK);
+	}
+
 	@Operation(summary = "이미지 업로드 API", description = "multipartForm으로 업로드. 업로드 후 응답받은 string 값을 통해 이미지를 로드 가능합니다.")
 	@PostMapping(value = "/api/v1/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@Authorization(openedForAll = true)
@@ -70,5 +88,17 @@ public class ImageController {
 			throw new IllegalArgumentException();
 		}
 		return fullFileName.substring(extensionIdx + 1);
+	}
+
+	private HttpHeaders getDownloadHeaders(final String fileRealName) {
+		final HttpHeaders headers = new HttpHeaders();
+		headers.setContentDisposition(
+			ContentDisposition.attachment()
+				.filename(
+					fileRealName,
+					StandardCharsets.UTF_8).build()
+		);
+
+		return headers;
 	}
 }
