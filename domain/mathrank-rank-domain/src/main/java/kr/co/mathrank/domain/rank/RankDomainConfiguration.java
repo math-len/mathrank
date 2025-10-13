e6a6192bc80736dcf2bb67e0a752b1127ac367be
huhdy32 <mong3268@gmail.com>
2025-10-13 12:09:56 +0900

    refactor(rank): mysql 을 통해 랭크 조회하도록 수정

--- domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/RankDomainConfiguration.java
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/RankDomainConfiguration.java
@@ -1,9 +1,35 @@
 package kr.co.mathrank.domain.rank;
 
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
+
+import kr.co.mathrank.common.cache.RequiredCacheSpec;
 
 @EnableRetry
 @Configuration
 public class RankDomainConfiguration {
+	public static final String USER_RANK_CACHE_NAME = "mathrank::domain::rank::user";
+
+	@Bean
+	RequiredCacheSpec requiredCacheSpec() {
+		return new RequiredCacheSpec() {
+			@Override
+			public String moduleName() {
+				return "mathrank-domain-rank";
+			}
+
+			@Override
+			public String cacheName() {
+				return USER_RANK_CACHE_NAME;
+			}
+
+			@Override
+			public Duration ttl() {
+				return Duration.ofSeconds(10);
+			}
+		};
+	}
 }
