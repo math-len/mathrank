14256a586efa1607fa0af41eeb593bcdbfd0e241
huhdy32 <mong3268@gmail.com>
2025-10-13 11:22:15 +0900

    feat(assessment): 정답 조회 시, 관리자면 풀이여부에 상관없이 조회되도록 수정

--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/dto/AssessmentSolutionQuery.java
+++ domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/dto/AssessmentSolutionQuery.java
@@ -1,11 +1,14 @@
 package kr.co.mathrank.domain.problem.assessment.dto;
 
 import jakarta.validation.constraints.NotNull;
+import kr.co.mathrank.common.role.Role;
 
 public record AssessmentSolutionQuery(
 	@NotNull
 	Long assessmentId,
 	@NotNull
-	Long requestMemberId
+	Long requestMemberId,
+	@NotNull
+	Role requestMemberRole
 ) {
 }
--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/service/AssessmentSolutionQueryService.java
+++ domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/service/AssessmentSolutionQueryService.java
@@ -7,13 +7,14 @@ import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import kr.co.mathrank.client.internal.problem.ProblemQueryResult;
+import kr.co.mathrank.common.role.Role;
 import kr.co.mathrank.domain.problem.assessment.dto.AssessmentSolutionQuery;
 import kr.co.mathrank.domain.problem.assessment.dto.AssessmentSolutionQueryResult;
 import kr.co.mathrank.domain.problem.assessment.dto.ProblemSolutionResult;
 import kr.co.mathrank.domain.problem.assessment.entity.Assessment;
 import kr.co.mathrank.domain.problem.assessment.entity.AssessmentItem;
 import kr.co.mathrank.domain.problem.assessment.exception.CannotGetSolutionException;
+import kr.co.mathrank.domain.problem.assessment.exception.NoSuchSubmissionException;
 import kr.co.mathrank.domain.problem.assessment.repository.AssessmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,7 +32,7 @@ public class AssessmentSolutionQueryService {
 	 * @param query
 	 */
 	public AssessmentSolutionQueryResult querySolutions(@NotNull @Valid final AssessmentSolutionQuery query) {
-		final Assessment solvedAssessment = getSubmittedAssessment(query.assessmentId(), query.requestMemberId());
+		final Assessment solvedAssessment = getSubmittedAssessment(query.assessmentId(), query.requestMemberId(), query.requestMemberRole());
 
 		final List<ProblemSolutionResult> results = solvedAssessment.getAssessmentItems().stream()
 			.map(AssessmentItem::getProblemId)
@@ -42,13 +43,33 @@ public class AssessmentSolutionQueryService {
 		return new AssessmentSolutionQueryResult(results);
 	}
 
-	private Assessment getSubmittedAssessment(final Long assessmentId, final Long requestMemberId) {
-		return assessmentRepository.findByAssessmentIdAndSubmissionMemberId(assessmentId, requestMemberId)
+	private Assessment getSubmittedAssessment(final Long assessmentId, final Long requestMemberId, final Role role) {
+		return assessmentRepository.findWithSubmissions(assessmentId).stream()
+			.peek(assessment -> validatePermission(assessment, requestMemberId, role))
+			.findAny()
 			.orElseThrow(() -> {
 				log.info(
-					"[AssessmentSolutionQueryService.querySolutions] assessment not solved - assessmentId: {}, requestMemberId: {}",
+					"[AssessmentSolutionQueryService.getSubmittedAssessment] assessment not solved - assessmentId: {}, requestMemberId: {}",
 					assessmentId, requestMemberId);
-				return new CannotGetSolutionException();
+				return new NoSuchSubmissionException();
 			});
+	}
+
+	private void validatePermission(final Assessment assessment, final Long requestMemberId, final Role role) {
+		// 관리자면 pass
+		if (role == Role.ADMIN) {
+			return;
+		}
+
+		// 풀이기록이 있을때 통과
+		if (assessment.getAssessmentSubmissions().stream()
+			.anyMatch(assessmentSubmission -> assessmentSubmission.getMemberId().equals(requestMemberId))) {
+			return;
+		}
+
+		log.info(
+			"[AssessmentSolutionQueryService.validatePermission] cannot access assessment solution - assessmentId: {}, requestMemberId: {}, requestMemberRole: {}",
+			assessment.getId(), requestMemberId, role);
+		throw new CannotGetSolutionException();
 	}
 }
