990be3ece3dbf6722e51df5be50b2564d1ea4043
huhdy32 <mong3268@gmail.com>
2025-10-14 16:17:06 +0900

    feat(rank): 인덱스 추가

--- domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/entity/Solver.java
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/entity/Solver.java
@@ -21,7 +21,8 @@ import lombok.Setter;
 @Table(
 	indexes = {
 		@Index(name = "idx_score_memberId", columnList = "score desc, member_id"),
-		@Index(name = "idx_memberId_score", columnList = "member_id, score desc")
+		@Index(name = "idx_memberId_score", columnList = "member_id, score desc"),
+		@Index(name = "idx_schoolCode_score", columnList = "school_code, score")
 	}
 )
 @Getter
