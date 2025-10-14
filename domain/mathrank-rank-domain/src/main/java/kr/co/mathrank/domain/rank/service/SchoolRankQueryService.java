c9cb499ac13d7e358ccbbfa461fbb8915c974b4a
huhdy32 <mong3268@gmail.com>
2025-10-14 16:27:45 +0900

    feat(rank): 학교 랭킹 조회에 캐싱 적용

--- domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/SchoolRankQueryService.java
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/SchoolRankQueryService.java
@@ -2,6 +2,8 @@ package kr.co.mathrank.domain.rank.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -9,6 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import jakarta.validation.constraints.NotNull;
 import kr.co.mathrank.common.page.PageResult;
 import kr.co.mathrank.common.page.PageUtil;
+import kr.co.mathrank.domain.rank.RankDomainConfiguration;
 import kr.co.mathrank.domain.rank.dto.SchoolRankQueryResult;
 import kr.co.mathrank.domain.rank.repository.SolverRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +19,14 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 @Validated
+@CacheConfig(cacheNames = RankDomainConfiguration.SCHOOL_RANK_BOARD_CACHE_NAME)
 public class SchoolRankQueryService {
 	private final SolverRepository solverRepository;
 
+	@Cacheable(
+		key = "'pageNumber::' + #pageNumber + '::pageSize::' + #pageSize",
+		condition = "#pageNumber <= 4"
+	)
 	public PageResult<SchoolRankQueryResult> querySchoolRanks(
 		@NotNull final Integer pageSize,
 		@NotNull final Integer pageNumber
