9fdb01d61d8d55591b9c56f1d41d437b1b16dfa6
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-10-15 15:16:13 +0900

    Revert "feat(point): 이벤트 기반 포인트 소비 기능 구성 (#229)"
    This reverts commit bb240f7d0fa1ed255f5649278ff13785a29621d4.

--- domain/mathrank-point-domain/src/main/java/kr/co/mathrank/domain/point/repository/UserPointRepository.java
+++ domain/mathrank-point-domain/src/main/java/kr/co/mathrank/domain/point/repository/UserPointRepository.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import jakarta.persistence.LockModeType;
 import kr.co.mathrank.domain.point.entity.UserPoint;
@@ -13,11 +11,4 @@ import kr.co.mathrank.domain.point.entity.UserPoint;
 public interface UserPointRepository extends JpaRepository<UserPoint, Long> {
 	@Lock(LockModeType.OPTIMISTIC)
 	Optional<UserPoint> findByUserId(Long userId);
-
-	@Query("""
-SELECT u FROM UserPoint u
-WHERE u.userId = :memberId
-""")
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
-	Optional<UserPoint> findByUserIdForUpdate(@Param("memberId") Long memberId);
 }
