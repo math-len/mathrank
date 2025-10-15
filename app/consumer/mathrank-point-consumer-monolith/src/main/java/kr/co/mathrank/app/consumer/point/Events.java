c72a2c1d75255ff64f2048bfc728ba8260b55f99
huhdy32 <mong3268@gmail.com>
2025-10-15 13:19:07 +0900

    feat(point): 이벤트 페이로드에 필드 추가

--- app/consumer/mathrank-point-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/point/Events.java
+++ app/consumer/mathrank-point-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/point/Events.java
@@ -6,6 +6,7 @@ import kr.co.mathrank.common.event.EventPayload;
 
 public class Events {
 	record ContentOrderRegisteredEvent(
+		Long orderId,
 		Long memberId,
 		Long contentId,
 		Long contentPointCost,
