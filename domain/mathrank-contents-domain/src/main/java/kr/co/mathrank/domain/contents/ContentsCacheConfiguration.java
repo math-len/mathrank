6d2ddf2af545e987f931c9d9227af6778ebc0440
huhdy32 <mong3268@gmail.com>
2025-10-15 16:22:35 +0900

    feat(contents): 자료 목록 조회 캐싱 적용

--- domain/mathrank-contents-domain/src/main/java/kr/co/mathrank/domain/contents/ContentsCacheConfiguration.java
+++ domain/mathrank-contents-domain/src/main/java/kr/co/mathrank/domain/contents/ContentsCacheConfiguration.java
@@ -10,6 +10,27 @@ import kr.co.mathrank.common.cache.RequiredCacheSpec;
 @Configuration
 public class ContentsCacheConfiguration {
 	public static final String CONTENT_ORDER_PAGE_CACHE_NAME = "mathrank-content-order-page";
+	public static final String CONTENT_LIST_PAGE_CACHE_NAME = "mathrank-content-page";
+
+	@Bean
+	RequiredCacheSpec contentPageCache() {
+		return new RequiredCacheSpec() {
+			@Override
+			public String moduleName() {
+				return "mathrank-content-domain";
+			}
+
+			@Override
+			public String cacheName() {
+				return CONTENT_LIST_PAGE_CACHE_NAME;
+			}
+
+			@Override
+			public Duration ttl() {
+				return Duration.ofMinutes(1);
+			}
+		};
+	}
 
 	@Bean
 	RequiredCacheSpec contentOrderPageCache() {
