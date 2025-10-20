c61a49c9696be4ce8096c77d1350b7436159d433
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-10-20 20:37:16 +0900

    Revert "feat(oauth): 회원탈퇴시 oauth 연동 해제되도록 구현 (#258)" (#260)
    This reverts commit 542e7cd57d8186f2cf86fb1c8c86f2131e73302e.

--- domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/client/OAuthClientHandler.java
+++ domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/client/OAuthClientHandler.java
@@ -6,5 +6,4 @@ import kr.co.mathrank.domain.auth.entity.OAuthProvider;
 interface OAuthClientHandler {
 	MemberInfoResponse getMemberInfo(final OAuthLoginCommand command);
 	boolean supports(final OAuthProvider provider);
-	boolean revoke(final String refreshToken);
 }
