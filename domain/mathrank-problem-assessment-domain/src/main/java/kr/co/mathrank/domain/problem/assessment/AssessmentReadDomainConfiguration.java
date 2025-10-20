2d9d7fff8f021fbb5061176178e310ad020de4ba
huhdy32 <mong3268@gmail.com>
2025-10-20 12:16:20 +0900

    feat(assessment): 캐싱 시간 수정

--- domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/AssessmentReadDomainConfiguration.java
+++ domain/mathrank-problem-assessment-domain/src/main/java/kr/co/mathrank/domain/problem/assessment/AssessmentReadDomainConfiguration.java
@@ -21,45 +21,47 @@ public class AssessmentReadDomainConfiguration {
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainProblemCacheSpec() {
-		return createCacheSpec(PROBLEM_CACHE_NAME, Duration.ofSeconds(60));
+		return createCacheSpec(PROBLEM_CACHE_NAME, Duration.ofSeconds(5L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainCourseCacheSpec() {
-		return createCacheSpec(COURSE_CACHE_NAME, Duration.ofSeconds(60));
+		return createCacheSpec(COURSE_CACHE_NAME, Duration.ofMinutes(1L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentCacheSpec() {
-		return createCacheSpec(ASSESSMENT_READ_MODEL_CACHE_NAME, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_READ_MODEL_CACHE_NAME, Duration.ofSeconds(5L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentSingleCacheSpec() {
-		return createCacheSpec(ASSESSMENT_READ_SINGLE_CACHE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_READ_SINGLE_CACHE, Duration.ofSeconds(5L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentPageCacheSpec() {
-		return createCacheSpec(ASSESSMENT_READ_PAGE_CAHCE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_READ_PAGE_CAHCE, Duration.ofSeconds(3L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentRankCacheSpec() {
-		return createCacheSpec(ASSESSMENT_READ_RANK_CACHE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_READ_RANK_CACHE, Duration.ofSeconds(3L));
 	}
+
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentSolutionCacheSpec() {
-		return createCacheSpec(ASSESSMENT_READ_SOLUTION_CACHE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_READ_SOLUTION_CACHE, Duration.ofSeconds(10));
 	}
+
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentStatisticsCacheSpec() {
-		return createCacheSpec(ASSESSMENT_STATISTICS_CACHE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_STATISTICS_CACHE, Duration.ofSeconds(10L));
 	}
 
 	@Bean
 	RequiredCacheSpec assessmentReadDomainAssessmentSingleSubmissionCacheSpec() {
-		return createCacheSpec(ASSESSMENT_SUBMISSION_SINGLE_CACHE, Duration.ofSeconds(60));
+		return createCacheSpec(ASSESSMENT_SUBMISSION_SINGLE_CACHE, Duration.ofMinutes(1L));
 	}
 
 	private RequiredCacheSpec createCacheSpec(final String cacheName, final Duration ttl) {
