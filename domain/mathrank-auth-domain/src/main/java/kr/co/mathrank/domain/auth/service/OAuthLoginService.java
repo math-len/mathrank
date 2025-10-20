deeffdd9063ec03f282c0bdd4f34368af151565d
huhdy32 <mong3268@gmail.com>
2025-10-20 17:01:39 +0900

    feat(oauth): 로그인 마다 저장된 갱신 토큰 갱신하도록 수정

--- domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/service/OAuthLoginService.java
+++ domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/service/OAuthLoginService.java
@@ -15,6 +15,7 @@ import kr.co.mathrank.domain.auth.dto.JwtOAuthLoginResult;
 import kr.co.mathrank.domain.auth.dto.MemberInfoCompleteCommand;
 import kr.co.mathrank.domain.auth.dto.OAuthLoginCommand;
 import kr.co.mathrank.domain.auth.entity.Member;
+import kr.co.mathrank.domain.auth.entity.OAuthInfo;
 import kr.co.mathrank.domain.auth.exception.CannotFoundMemberException;
 import kr.co.mathrank.domain.auth.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -49,6 +50,7 @@ public class OAuthLoginService {
 				return newMember;
 			});
 
+		refreshTokenInfo(memberInfo, member);
 		log.info("[OAuthLoginService.login] member login success with oAuth - memberId: {}, oAuthProvider: {}", member.getId(), member.getOAuthInfo().getOAuthProvider());
 		// 사용자의 등록 지연 여부를 함께 응답한다.
 		return JwtOAuthLoginResult.from(jwtLogin(member), member.getPending());
@@ -76,5 +78,10 @@ public class OAuthLoginService {
 
 	private JwtLoginResult jwtLogin(final Member member) {
 		return jwtLoginManager.login(member.getId(), member.getRole(), member.getName());
+	}
+
+	private void refreshTokenInfo(final MemberInfo memberInfo, final Member member) {
+		final OAuthInfo oAuthInfo = member.getOAuthInfo();
+		oAuthInfo.setOAuthRefreshToken(memberInfo.refreshToken());
 	}
 }
