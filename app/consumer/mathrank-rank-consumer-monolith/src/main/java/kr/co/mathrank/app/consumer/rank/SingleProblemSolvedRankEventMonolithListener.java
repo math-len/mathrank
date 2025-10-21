f7fe633a199f9e6134bdf6db6abadde7e3e30cad
huhdy32 <mong3268@gmail.com>
2025-10-21 11:29:27 +0900

    feat(rank): 사용자 업데이트 이벤트 컨슈머 추가

--- app/consumer/mathrank-rank-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/rank/SingleProblemSolvedRankEventMonolithListener.java
+++ app/consumer/mathrank-rank-consumer-monolith/src/main/java/kr/co/mathrank/app/consumer/rank/SingleProblemSolvedRankEventMonolithListener.java
@@ -1,19 +1,24 @@
 package kr.co.mathrank.app.consumer.rank;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import kr.co.mathrank.common.event.Event;
 import kr.co.mathrank.common.event.publisher.monolith.MonolithEvent;
+import kr.co.mathrank.domain.rank.dto.SolverUpdateCommand;
 import kr.co.mathrank.domain.rank.service.SolveLogRegisterService;
+import kr.co.mathrank.domain.rank.service.SolverUpdateService;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 class SingleProblemSolvedRankEventMonolithListener {
 	private final SolveLogRegisterService solveLogRegisterService;
+	private final SolverUpdateService solverUpdateService;
 
 	@EventListener
+	@Async
 	public void consume(final MonolithEvent monolithEvent) {
 		if (!monolithEvent.isExpectedTopic("single-problem-solved")) {
 			return;
@@ -24,5 +29,19 @@ class SingleProblemSolvedRankEventMonolithListener {
 				EventPayloads.SingleProblemSolvedEventPayload.class)
 			.getPayload();
 		solveLogRegisterService.register(payload.toCommand());
+	}
+
+	@EventListener
+	@Async
+	public void consumeMemberUpdatedEvent(final MonolithEvent monolithEvent) {
+		if (!monolithEvent.isExpectedTopic("mathrank-member-updated")) {
+			return;
+		}
+		final EventPayloads.MemberUpdatedEventPayload payload = Event.fromJson(
+			monolithEvent.payload(),
+			EventPayloads.MemberUpdatedEventPayload.class
+		).getPayload();
+
+		solverUpdateService.updateSolver(new SolverUpdateCommand(Long.parseLong(payload.memberId()), payload.name(), payload.schoolCode()));
 	}
 }
