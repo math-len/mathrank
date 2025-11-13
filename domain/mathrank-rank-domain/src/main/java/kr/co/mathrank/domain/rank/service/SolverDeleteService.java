569d473af24e3b28700864c4c34f9c854a84eb8f
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-11-13 14:03:27 +0900

    feat(rank): 사용자 삭제됐을때, 랭킹에서도 삭제되도록 수정 (#273)
    * feat(rank): 랭커 삭제 기능 구현
    
    * feat(member): 사용자 삭제 시, 이벤트 발행하도록 구현
    
    * feat(rank): 멤버 삭제 이벤트 수신해서 랭킹에서 제거하도록 수정
    
    * refactor(rank): 사용자 찾지 못했을 때, 로그만 남기도록 수정
    
    * fix(rank): 사용자 id 로 찾도록 수정

---
+++ domain/mathrank-rank-domain/src/main/java/kr/co/mathrank/domain/rank/service/SolverDeleteService.java
@@ -0,0 +1,30 @@
+package kr.co.mathrank.domain.rank.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import kr.co.mathrank.domain.rank.repository.SolverRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Validated
+@RequiredArgsConstructor
+public class SolverDeleteService {
+	private final SolverRepository solverRepository;
+
+	@Transactional
+	public void delete(@NotNull @Valid final Long memberId) {
+		solverRepository.findByMemberId(memberId)
+			.ifPresentOrElse(solver -> {
+				solverRepository.delete(solver);
+				log.info("[SolverDeleteService.delete] solver deleted - memberId: {}", memberId);
+			}, () -> {
+				log.info("[SolverDeleteService.delete] cannot found solver - memberId: {}", memberId);
+			});
+	}
+}
