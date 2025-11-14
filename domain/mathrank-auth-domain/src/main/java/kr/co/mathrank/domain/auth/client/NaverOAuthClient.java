fa6868e7efaec40b824093a9d4863a6f9b981259
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2025-11-14 14:14:14 +0900

    Revert "fix(oauth): naver oauth token revocation에서 인코딩으로 인한 에러 수정 (#275)" (#276)
    This reverts commit 996405a8d7383ccd58d4a946922cbc4c62fa7894.

--- domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/client/NaverOAuthClient.java
+++ domain/mathrank-auth-domain/src/main/java/kr/co/mathrank/domain/auth/client/NaverOAuthClient.java
@@ -1,12 +1,8 @@
 package kr.co.mathrank.domain.auth.client;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriUtils;
 
 import kr.co.mathrank.domain.auth.dto.OAuthLoginCommand;
 import kr.co.mathrank.domain.auth.entity.OAuthProvider;
@@ -68,7 +64,7 @@ class NaverOAuthClient implements OAuthClientHandler {
 			.uri(uriBuilder -> uriBuilder
 				.queryParam("client_id", naverConfiguration.getClientId())
 				.queryParam("client_secret", naverConfiguration.getClientSecret())
-				.queryParam("access_token", UriUtils.encode(token.access_token(), StandardCharsets.UTF_8))
+				.queryParam("access_token", token.access_token())
 				.queryParam("grant_type", "delete")
 				.queryParam("service_provider", "NAVER")
 				.build())
