2dac2cef4d6b036436c9bd8ae6d8402965ea796b
Heejong Yoon <101308287+huhdy32@users.noreply.github.com>
2026-01-09 16:08:29 +0900

    feat(lazy-connection): lazyConnectionDataSourceProxy 빈 통해 커넥션 점유기간 최소화 (#278)
    * feat(lazy-connection): lazyConnectionDataSourceProxy 추가를 통해 커넥션 점유 최소화
    
    * style(lazy-connection): import 정리

---
+++ common/mathrank-lazy-connection/src/main/java/kr/co/mathrank/common/lazyconnection/LazyConnectionDataSourceProxyConfiguration.java
@@ -0,0 +1,27 @@
+package kr.co.mathrank.common.lazyconnection;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+class LazyConnectionDataSourceProxyConfiguration {
+	@Bean
+	HikariDataSource hikariDataSource(final DataSourceProperties dataSourceProperties) {
+		return dataSourceProperties.initializeDataSourceBuilder()
+			.type(HikariDataSource.class)
+			.build();
+	}
+
+	@Bean
+	@Primary
+	DataSource lazyConnectionDataSource(final HikariDataSource hikariDataSource) {
+		return new LazyConnectionDataSourceProxy(hikariDataSource);
+	}
+}
